a6f6c6f6291df6ecd5b1681b7289a45e55e97b76
Sames Davis <samesdavis@outlook.com>
2017-01-20 00:26:05 -0200

    Projeto Inicial

---
+++ ControledeEstoque.Web/Controllers/GraficoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControledeEstoque.Web.Controllers
+{
+    public class GraficoController : Controller
+    {
+        [Authorize]
+        public ActionResult PerdasMensais()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult EntradaSaidaMesa()
+        {
+            return View();
+        }
+    }
+}
---
+++ ControledeEstoque.Web/Models/UsuarioModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ControledeEstoque.Web.Models
+{
+    public class UsuarioModel
+    {
+        public static bool ValidarUsuario(string Login, string senha)
+        {
+            bool ret = false;
+            using (var conexao = new SqlConnection())
+            {
+                conexao.ConnectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=EstoqueWeb;Integrated Security=True";
+                conexao.Open();
+                using (var comando = new SqlCommand())
+                {
+                    comando.Connection = conexao;
+                    comando.CommandText = string.Format("SELECT count(*) from usuarios where login = '{0}' and senha = '{1}';", Login, senha);
+                    ret = ((int)comando.ExecuteScalar() > 0);
+                }
+                conexao.Close();
+            }
+            return ret;
+        }
+    }
+}
